8118e303623cbca4f5291c578ab83484b0b31368
Aline Buchino <aline.buchino@hotmail.com>
2022-06-07 22:27:00 -0300

    Payment, one-to-one association

--- src/main/java/com/example/courseSpring/config/TestConfig.java
+++ src/main/java/com/example/courseSpring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.courseSpring.entities.Category;
 import com.example.courseSpring.entities.Order;
 import com.example.courseSpring.entities.OrderItem;
+import com.example.courseSpring.entities.Payment;
 import com.example.courseSpring.entities.Product;
 import com.example.courseSpring.entities._User;
 import com.example.courseSpring.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
